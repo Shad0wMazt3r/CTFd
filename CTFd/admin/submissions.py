0f0644582d3dff9bc2eafba6526cf6f66b087668
Pratyaksha Beri <pberi@iastate.edu>
2025-10-21 11:42:45 -0500

    submissions fixed

--- CTFd/admin/submissions.py
+++ CTFd/admin/submissions.py
@@ -1,10 +1,11 @@
 from flask import render_template, request, url_for
 
 from CTFd.admin import admin
-from CTFd.models import Challenges, Submissions
+from CTFd.models import Challenges, Submissions, Users, Teams
 from CTFd.utils.decorators import admins_only
 from CTFd.utils.helpers.models import build_model_filters
 from CTFd.utils.modes import get_model
+from CTFd.utils.config import is_hybrid_mode
 
 
 @admin.route("/admin/submissions", defaults={"submission_type": None})
@@ -32,14 +33,37 @@ def submissions_listing(submission_type):
 
     Model = get_model()
 
-    submissions = (
-        Submissions.query.filter_by(**filters_by)
-        .filter(*filters)
-        .join(Challenges)
-        .join(Model)
-        .order_by(Submissions.date.desc())
-        .paginate(page=page, per_page=50, error_out=False)
-    )
+    if is_hybrid_mode():
+        # In hybrid mode, we need to handle both users and teams
+        # Use outerjoin to get submissions from both individual users and teams
+        submissions = (
+            Submissions.query.filter_by(**filters_by)
+            .filter(*filters)
+            .join(Challenges)
+            .outerjoin(Users, Submissions.user_id == Users.id)
+            .outerjoin(Teams, Submissions.team_id == Teams.id)
+            .order_by(Submissions.date.desc())
+            .paginate(page=page, per_page=50, error_out=False)
+        )
+    else:
+        if Model:
+            submissions = (
+                Submissions.query.filter_by(**filters_by)
+                .filter(*filters)
+                .join(Challenges)
+                .join(Model)
+                .order_by(Submissions.date.desc())
+                .paginate(page=page, per_page=50, error_out=False)
+            )
+        else:
+            # Fallback for cases where Model is None (shouldn't happen in non-hybrid modes)
+            submissions = (
+                Submissions.query.filter_by(**filters_by)
+                .filter(*filters)
+                .join(Challenges)
+                .order_by(Submissions.date.desc())
+                .paginate(page=page, per_page=50, error_out=False)
+            )
 
     args = dict(request.args)
     args.pop("page", 1)
